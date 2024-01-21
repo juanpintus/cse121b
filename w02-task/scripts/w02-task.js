644acaa6bb4b2c6cce383c07a06343d3334353ee
JuanPintus <juan.pintus@gmail.com>
2024-01-20 21:12:34 -0300

    update

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,26 +4,47 @@
 
 /* Step 2 - Variables */
 
-
-
+let fullName = 'Juan F. Pintus';
+let currentYear = '2024';
+let profilePicture = '/profilephoto.png';
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
 
+const foodElement = document.getElementById('food');
 
+const yearElement = document.querySelector('#year');
 
+const imageElement = document.getElementById('image');
 
 /* Step 4 - Adding Content */
 
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
+yearElement.textContent = currentYear;
 
+imageElement.setAttribute('src', profilePicture);
 
-
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
+let favoriteFoods = ['Milanesas', 'Cereal', 'Empanadas'];
+
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+let newFavoriteFood = 'Potato Fries';
 
 
+favoriteFoods.push(newFavoriteFood);
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
+favoriteFoods.shift();
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+favoriteFoods.pop();
+
+foodElement.innerHTML += `<br>${favoriteFoods}`;

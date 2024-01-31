acb75fd236a127b58f8e90cc49f912f654fa2c77
JuanPintus <juan.pintus@gmail.com>
2024-01-31 00:26:29 -0300

    w03Task completed

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,19 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+// console.log("TESTS: 123");
+// let scores = [56, 78, 95, 77, 65, 86];
+// let target = scores.indexOf(87);
+// // console.log(target);
+
+// const points = [100,88,94,78,84,69];
+
+// function convertPointsToStatus(pointTotal) {
+//   let status = 'Not Complete';
+//   if (pointTotal >= 93) {
+//     status = 'Excellent';
+//   } else if (pointTotal >= 70) {
+//     status = 'Complete';
+//   }
+//   return status;
+// }
